b96ea72d62e82220398b533b364246f03c320bcd
Geoffrey O'Connor <gaoconnor@partners.org>
2023-02-09 09:50:04 -0500

    add isHealthy and toggleHealth methods

--- Program.cs
+++ Program.cs
@@ -1,6 +1,16 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
+bool isHealthy = true;
 
 app.MapGet("/", () => "Hello World!");
+
+app.MapGet("/toggleHealth", () => {
+    isHealthy = !isHealthy;
+    return isHealthy;
+} );
+
+app.MapGet("/isHealthy", () => {
+    return isHealthy ? StatusCodes.Status200OK : StatusCodes.Status400BadRequest;
+});
 
 app.Run();
