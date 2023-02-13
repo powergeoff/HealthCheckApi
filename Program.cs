4baa657150868ac2ae2786ffce0c91b0f5f470d1
Geoffrey O'Connor <gaoconnor@partners.org>
2023-02-13 06:15:26 -0500

    additional logging

--- Program.cs
+++ Program.cs
@@ -5,23 +5,24 @@ var app = builder.Build();
 
 bool isHealthy = true;
 
-app.Logger.LogInformation("Adding Routes");
+app.Logger.LogInformation(DateTime.Now.ToString() +  " Adding Routes");
 app.MapGet("/", () => "Hello World!");
 
 app.MapGet("/toggleHealth", () => {
     isHealthy = !isHealthy;
-    app.Logger.LogInformation("/toggleHealth invoked. Variable = " + isHealthy);
+    app.Logger.LogInformation(DateTime.Now.ToString() +  " Toggle invoked. Variable = " + isHealthy);
     return isHealthy;
 } );
 
 
 app.MapGet("/isHealthy", () => {
+    app.Logger.LogInformation(DateTime.Now.ToString() + " Health Check Invoked isHealthy = " + isHealthy);
     return isHealthy ? new HttpResponseMessage(System.Net.HttpStatusCode.OK) : throw new BadHttpRequestException("app not healthy", 500);
 });
 
 //app.UseHttpsRedirection();
 
-app.Logger.LogInformation("Starting the app");
+app.Logger.LogInformation(DateTime.Now.ToString() +  " Starting the app");
 app.Run();
 
 
